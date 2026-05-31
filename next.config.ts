85c09946dad2e8b60915d6f485702ae7e528fcea
aiymnn <aimanazizan04@gmail.com>
2026-05-31 23:32:33 +0800

    UPDATE: fix image loop

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.9"],
+  images: {
+    qualities: [55, 75],
+  },
 };
 
 export default nextConfig;
